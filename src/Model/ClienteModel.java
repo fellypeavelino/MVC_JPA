8b10f0d43d338b670cf2c28955c4b86b4121ada4
fellypeavelino <fellypeavelino@hotmail.com>
2014-10-09 21:35:44 -0300

    segundo commit

--- src/Model/ClienteModel.java
+++ src/Model/ClienteModel.java
@@ -12,8 +12,10 @@ import Interfaces.IClienteModel;
 public class ClienteModel extends Base<Cliente, Integer> implements IClienteModel{
 
 	public ArrayList<Cliente> listarClienteComDebito(Debito debito) throws  Exception{
-		TypedQuery<Cliente> Clientes = this.em.createQuery("Select c.cliente from Debito c where c.status = "+ debito.getStatus(), Cliente.class);
+		TypedQuery<Cliente> Clientes = this.em.createQuery("Select c.cliente from Debito c where c.status = "+ debito.getStatus()+" and c.vencimento = "+debito.getVencimento(), Cliente.class);
 		ArrayList<Cliente> listaClientes = (ArrayList<Cliente>) Clientes.getResultList();
 		return listaClientes;
 	}
+	
+	
 }
